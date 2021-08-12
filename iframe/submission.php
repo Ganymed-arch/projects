77b42dc40dc6143de2365b658641b38976d3a851
MaxBuhe01 <mb@shopmodule.com>
2021-08-12 19:13:28 +0200

    removed required and patterns

--- iframe/submission.php
+++ iframe/submission.php
@@ -17,19 +17,19 @@
                 <form method="POST" action="thank_you.php">
                     <label class="col-12 m-1" id="username">
                         ⮚ Enter here your specific Discord Username:&nbsp;<br>
-                        <input class="w-100" required pattern="[^\s-].*#[0-9]{4}" minlength="1" maxlength="100" type="text" content="user-name" name="user-name" placeholder="Example#1234">
+                        <input class="w-100" minlength="1" maxlength="100" type="text" content="user-name" name="user-name" placeholder="Example#1234">
                     </label>
                     <label class="col-12 m-1">
                         ⮚ Enter here the amount of users on your server:&nbsp;
-                        <input class="w-100" required min="1" max="1000000" type="number" content="user-count" name="user-count" placeholder="Please use the general amount, no special letters">
+                        <input class="w-100" min="1" max="1000000" type="number" content="user-count" name="user-count" placeholder="Please use the general amount, no special letters">
                     </label>
                     <label class="col-12 m-1">
                         ⮚ Please do also add an invitation to your server:&nbsp;
-                        <input class="w-100" type="text" content="invitation" required minlength="1" maxlength="100" pattern="^https:\/\/discord\.gg\/[A-za-z0-9]{1,10}$" name="invitation" placeholder="https://discord.gg/tALu2Cx">
+                        <input class="w-100" type="text" content="invitation"  minlength="1" maxlength="100" name="invitation" placeholder="https://discord.gg/tALu2Cx">
                     </label>
                     <label class="col-12 m-1">
                         ⮚ Enter here a short description about your server:&nbsp;
-                        <textarea class="w-100" style="height: 150px" content="server-description" required minlength="10" maxlength="1000" name="server-description" placeholder="Tell us what YOUR server makes it special..."></textarea>
+                        <textarea class="w-100" style="height: 150px" content="server-description"  minlength="10" maxlength="1000" name="server-description" placeholder="Tell us what YOUR server makes it special..."></textarea>
                     </label>
                     <label class="col-12 m-1">
                         <input type="submit" id="submit-button" class="w-100 btn bg-cust-blu text-light" value="SUBMIT!" content="submission-button" name="submission-button">
